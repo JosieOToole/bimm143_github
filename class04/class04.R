ba7062a9f091bf21558b6f585e8e8499f3249481
Josefina O'Toole <josefina@uname.net>
2025-02-25 11:39:50 -0800

    added more and more class content

---
+++ class04/class04.R
@@ -0,0 +1,11 @@
+#' ---
+#' title: "Class 4"
+#' author: "Josefina O'Toole (PID A16978557)"
+#' date: "January 16th, 2025"
+#' ---
+
+x <- 1:50
+plot(x, sin(x))
+
+# Make this nicer
+plot(x, sin(x), col="blue", type="l", lwd=4)
